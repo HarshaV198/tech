eb58c38e0aad9efb9bc1d2c27c52cb98b7133deb
harshav198 <harshav198@gmail.com>
2018-01-24 00:56:27 +0530

    fixes

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -47,6 +47,7 @@
 	<script src="{{ asset('admin/dist/js/adminlte.min.js')}}"></script>
 	<!-- AdminLTE for demo purposes -->
 	<script src="{{ asset('admin/dist/js/demo.js')}}"></script>
+	<script src="{{ asset('js/parsley.min.js')}}"></script>
 
 	@section('footerSection')
 
